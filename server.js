29f6f2abc7fa9c6351d91821eb878e75b0092d44
William Sousa <william.dsv@outlook.com>
2020-05-20 15:47:54 -0300

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 
 const app = express();
 const server = require("http").createServer(app);
@@ -14,6 +14,7 @@ const api = require("axios").create({
     baseURL: "https://goflix.azurewebsites.net/api"
 })
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'publish')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
